bf8297985b0ca53f27d1d78a1f5368f059562b7e
Rauane Lima <rauanelima7@gmail.com>
2023-09-01 21:20:16 -0300

    error

--- orientacao-a-objetos/bin/orientacao_a_objetos.dart
+++ orientacao-a-objetos/bin/orientacao_a_objetos.dart
@@ -4,6 +4,7 @@ import 'package:orientacao_a_objetos/module/pessoa_juridica.dart';
 import 'package:orientacao_a_objetos/services/EnviarNotificacao.dart';
 
 void main(List<String> arguments) {
+  print("Hello");
   var pessoaFisica1 = PessoaFisica("Rauane", "Rua lapadinha", "111.222.333-44", tipoNotificacao: TipoNotificacao.SMS);
   print(pessoaFisica1);
 
